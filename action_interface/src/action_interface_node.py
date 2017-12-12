88e3608c7a824dcc26de6f51495120ea0d99cf5e
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2017-12-12 17:41:09 +1300

    Add package uoa_bringup & bug fixed

--- action_interface/src/action_interface_node.py
+++ action_interface/src/action_interface_node.py
@@ -4,6 +4,7 @@
 import rospy
 import json
 from std_msgs.msg import String
+from mind_msgs.msg import Reply
 
 class ActionInterfaceNode:
     def __init__(self):
@@ -12,8 +13,10 @@ class ActionInterfaceNode:
         rospy.Subscriber('/perceptionResult', String, self.handle_perception_result)
         rospy.Subscriber('/taskExecution', String, self.handle_task_execution)
         self.pub_task_completed = rospy.Publisher('/taskComplition', String, queue_size=10)
-        rospy.Subscriber('/silbotComplition', String, self.handle_silbot_complition)
-        self.pub_silbot_execution = rospy.Publisher('/silbotExecution', String, queue_size=10)
+
+        self.silbot_task_requested = False
+        rospy.Subscriber('/scene_queue_empty', String, self.handle_silbot_complition)
+        self.pub_silbot_execution = rospy.Publisher('/reply', Reply, queue_size=10)
 
         rospy.loginfo('%s initialized...'%rospy.get_name())
 
@@ -23,21 +26,26 @@ class ActionInterfaceNode:
         rospy.loginfo('-'*20)
 
     def handle_silbot_complition(self, msg):
-        self.silbot_task_complition = True
+        if self.silbot_task_requested:
+            self.silbot_task_complition = True
 
     def handle_task_execution(self, msg):
         rospy.loginfo('-- task_execution received --')
 
-        req_task = {}
-        req_task['data'] = 'Hello! Nice to meet you.'
+        req_task = Reply()
+        req_task.header.stamp = rospy.Time.now()
+        req_task.reply = 'Hello! Nice to meet you. <br=0> Hello Again.'
 
         self.silbot_task_complition = False
-        self.pub_silbot_execution.publish(json.dumps(req_task))
+        rospy.sleep(0.2)
+        self.silbot_task_requested = True
+        self.pub_silbot_execution.publish(req_task)
         while not rospy.is_shutdown() and not self.silbot_task_complition:
             rospy.sleep(0.1)
 
         rospy.loginfo('-- task_execution completed --')
         self.pub_task_completed.publish(json.dumps({}))
+        self.silbot_task_requested = False
 
 
 if __name__ == '__main__':
